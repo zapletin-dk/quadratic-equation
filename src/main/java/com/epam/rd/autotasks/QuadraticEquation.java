f7950a563ffe9659ca0ca5a220589f204cef25a6
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-22 19:58:54 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -3,16 +3,22 @@ package com.epam.rd.autotasks;
 import java.util.Locale;
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
 public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
+        double d = b*b -4*a*c;
+        double v = 2 * a;
+        if (d > 0){
+            System.out.print((-b + Math.sqrt(d))/ v);
+            System.out.print(" " + (-b - Math.sqrt(d))/ v);
+        } else if (d == 0){
+            System.out.println(-b/v);
+        } else {
+            System.out.println("no roots");
+        }
 
     }
 
